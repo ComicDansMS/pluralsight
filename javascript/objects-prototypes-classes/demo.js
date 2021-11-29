22a6124e6fed8da46681fe21bfa3b36db4f2f2b2
Daniel <dan.r.holmes@gmail.com>
2021-11-30 08:27:14 +1030

    JavaScript Prototypes and Interfaces

--- javascript/objects-prototypes-classes/demo.js
+++ javascript/objects-prototypes-classes/demo.js
@@ -1,86 +1,59 @@
 'use strict'; 
 (function() {
 
-  let person ={
-    name: {
-      first: 'Daniel',
-      last: 'Holmes'
-    },
-    age: 35
-  };
+  function Person(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    Object.defineProperty(this, 'fullName', {
+      get: function() {
+        return this.firstName + ' ' + this.lastName
+      },
+      enumerable: true
+    });
+  }
+
+  function Student(firstName, lastName, age) {
+    Person.call(this, firstName, lastName, age);
+    this._enrolledCourses = [];
+
+    this.enroll = function(courseId) {
+      this._enrolledCourses.push(courseId)
+    };
+
+    this.getCourses = function() {
+      return this.fullName + "'s enrolled courses are: " + this._enrolledCourses.join(', ');
+    };
+  }
+  Student.prototype = Object.create(Person.prototype);
+  Student.prototype.constructor = Student;
+
+  let daniel = new Student('Daniel', 'Holmes', 35);
+
+  daniel.enroll('Learn to be cool');
+  daniel.enroll('Fly with the kids');
+  daniel.enroll('How to not give a fuck');
+  display(daniel);
   
-  Object.defineProperty(person, 'fullName',
-  {
-    get: function() {
-      return this.name.first + ' ' + this.name.last;
-    },
-    set: function(value) {
-      var nameParts = value.split(' ');
-      this.name.first = nameParts[0];
-      this.name.last = nameParts[1];
-    }
-  });
-
-  person.fullName = 'Dan Rhys';
-
-  display(person.fullName);
-  display(person.name.first);
-  display(person.name.last);
-
-
+  display(daniel.getCourses());
   
-
 })(); 
+
 
 /*
 
-RECAP OF KEY TAKEAWAYS
-======================
+KEY TAKEAWAYS
 
-- We can access properties with dot notation and also bracket notation
+- Functions have prototypes
+  - These prototypes point to objects in memory
+  - When you create new objects from those constructor functions, their prototypes point to the same object in memory. It's the same instance.
 
-  person.firstName;
-  person['firstName'];
+- Object prototypes can have properties
+- When asking for a property value of an object, it will check the object first and then the prototype if the object does not have the property.
+  - Any new objects created from constructor functions will inherit any properties set on the prototype, unless they have that property set.
 
+- We learnt how to setup inheritence with constructor functions by setting the prototype of one function by the prototype of another function.
+- We can initialise the parent classes properties by calling its function using the .call() function and passing through the context of 'this'
 
-- This allows us to use variables to access properties on objects
-
-  let propertyName = 'firstName';
-  person[propertyName];
-
-
-- We can use Object.defineProperty to make properties read-only, enumerable, and lock out changing property descriptors
-
-  Object.defineProperty(
-    person,
-    'firstName',
-    {writable: false}
-  );
-
-  Object.defineProprty(
-    person,
-    'firstName',
-    {enumerable: false}
-  );
-
-  Object.defineProperty(
-    person,
-    'firstName',
-    {configurable: false}
-  )
-
-
-  - We used Object.defineProperty to create getters and setter properties that are properties backed by functions
-
-  Object.defineProperty(
-    person,
-    'firstName',
-    get: function() {
-      ...
-    },
-    set: function() {
-      ...
-    }
-  );
 
 */
