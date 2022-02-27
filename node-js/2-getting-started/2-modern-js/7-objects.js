2d856ebf918de88af03f661368e14d207a760c10
Daniel <dan.r.holmes@gmail.com>
2022-02-27 16:49:11 +1030

    more node

--- node-js/2-getting-started/2-modern-js/7-objects.js
+++ node-js/2-getting-started/2-modern-js/7-objects.js
@@ -1,6 +1,23 @@
-const obj = {
-  p1: 10,
-  p2: 20,
-  f1() {},
-  f2: () => {},
-};
+// const obj = {
+//   p1: 10,
+//   p2: 20,
+//   f1() {},
+//   f2: () => {},
+// };
+
+
+// My code..
+
+const someVariable = 'potato';
+
+const PI = Math.PI;
+
+const bigObject = {
+  propertyOne: 20,
+  propertyTwo: 30,
+  [someVariable]: 355,
+  functionOne: () => { console.log('hello') },
+  PI
+}
+
+console.log(bigObject.PI);
--- node-js/2-getting-started/2-modern-js/8-destructuring.js
+++ node-js/2-getting-started/2-modern-js/8-destructuring.js
@@ -2,7 +2,7 @@
 // const E = Math.E;
 // const SQRT2 = Math.SQRT2;
 
-const { PI, E, SQRT2 }  = Math;
+// const { PI, E, SQRT2 }  = Math;
 
 // With require
 // const { readFile } = require('fs');
@@ -19,3 +19,21 @@ const { PI, E, SQRT2 }  = Math;
 // console.log(
 //   circleArea(circle)
 // );
+
+
+// My code..
+
+const { PI, E, SQRT2 } = Math;
+
+const circle = {
+    label: 'Dans circle',
+    radius: 69
+}
+
+const circleArea = ( { radius }, { precision = 4 } = {} ) => 
+    (PI * radius * radius ).toFixed(precision);
+
+console.log(
+    circleArea(circle, { precision: 5 })
+);
+
