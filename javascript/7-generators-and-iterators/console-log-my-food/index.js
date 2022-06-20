74f6e46a27186344f25ae30c848a224152086fb4
Daniel Holmes <dan.r.holmes@gmail.com>
2022-06-20 22:09:26 +0930

    add JS generators and iterators

---
+++ javascript/7-generators-and-iterators/console-log-my-food/index.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'enter command > '
+})
+
+readline.prompt();
+readline.on('line', line => {
+  switch (line.trim())
+})
+
+readline.question('What would you like to log today?', async item => {
+  const { data } = await axios.get('http://localhost:3001/food');
+  const it = data[Symbol.iterator]();
+  let position = it.next();
+
+  while(!position.done) {
+    const food = position.value.name;
+
+    if (food === item) {
+      console.log(`${item} has ${position.value.calories} calories`)
+    }
+
+    position = it.next();
+  }
+
+  console.log(item);
+  readline.close();
+});
