09c7f00977498160aaf009a0e163cf6920ac302e
Daniel Holmes <daniel.holmes@framecreative.com.au>
2022-06-21 08:58:17 +0930

    save

--- javascript/7-generators-and-iterators/console-log-my-food/index.js
+++ javascript/7-generators-and-iterators/console-log-my-food/index.js
@@ -8,7 +8,7 @@ const readline = require ('readline').createInterface({
 });
 
 readline.prompt();
-readline.on('line', line => {
+readline.on('line', async line => {
   switch (line.trim()) {
     case 'list vegan foods' :
       {
@@ -16,7 +16,7 @@ readline.on('line', line => {
           let idx = 0;
           const veganOnly = data.filter(food => {
             return food.dietary_preferences.includes('vegan');
-          })
+          });
 
           const veganIterable = {
             [Symbol.iterator]() {
@@ -27,40 +27,81 @@ readline.on('line', line => {
                   const current = veganOnly[idx];
                   idx++;
                   if (current) {
-                    return { value: current, done: false }
+                    return { value: current, done: false };
                   } else {
-                    return { value: current, done: true }
+                    return { value: current, done: true };
                   }
                 }
-              }
+              };
             }
-          }
+          };
 
           for (let val of veganIterable) {
             console.log(val.name);
           }
 
           readline.prompt();
-        })
+        });
       }
       break;
     case 'log' :
+      const { data } = await axios.get('http://localhost:3001/food');
+      const it = data[Symbol.iterator]();
+      let actionIt;
+
+      const actionIterator = {
+        [Symbol.iterator]() {
+          const positions = [...this.actions];
+          return {
+            [Symbol.iterator]() {
+              return this;
+            },
+            next(...args) {
+              if (positions.length > 0) {
+                const position = positions.shift();
+                const result = position(...args);
+                return { value: result, done: false };
+              } else {
+                return { value: result, done: true };
+              }
+            }
+          };
+        },
+        actions: [askForServingSize, displayCalories]
+      };
+
+      function askForServingSize(food) {
+        readline.question(
+          'How many servings did you eat? (as a decimal: 0.5, 1.0, 1.25, etc.)',
+          servingSize => {
+            actionIt.next(servingSize, food)
+          }
+        );
+      }
+
+      function displayCalories(servingSize, food) {
+        const calories = food.calories;
+        console.log(`${food.name} with a serving size of ${servingSize} has ${Number.parseFloat(calories) * parseInt(servingSize, 10).toFixed()} calories`);
+        actionIt.next();
+        readline.prompt();
+      }
+      
       {
-        readline.question('What would you like to log today?', async (item) => {
-          const { data } = await axios.get('http://localhost:3001/food');
-          const it = data[Symbol.iterator]();
+        readline.question('What would you like to log today?', (item) => {
           let position = it.next();
         
           while(!position.done) {
             const food = position.value.name;
             if (food === item) {
-              console.log(`${item} has ${position.value.calories} calories.`)
+              console.log(`${item} has ${position.value.calories} calories.`);
+              actionIt = actionIterator[Symbol.iterator]();
+              actionIt.next(position.value);
             }
             position = it.next();
           }
         
           readline.prompt();
-        })
+        });
       }
   }
 })
